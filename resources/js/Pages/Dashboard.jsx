2684a401fb48531655a22492e399d3090757ab16
Ashir-Sohail <sohailashir786@gmail.com>
2026-05-06 12:47:52 +0500

    updated

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Dashboard() {
+    return (
+        <h2>Welcome to the dashboard page</h2>
+    );
+}
+
+export default Dashboard;
---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</head>
+<body>
+    @inertia
+    @viteReactRefresh
+</body>
+</html>
